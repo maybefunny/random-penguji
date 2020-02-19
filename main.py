b1ec3742557310041b3011def2fa5375c8554158
Darfi Sultoni <darfisultoni@gmail.com>
2020-02-19 15:45:42 +0700

    ok

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import math
 import random
 import datetime
+from operator import itemgetter
 
 random.seed(datetime.datetime.now())
 
 asistens = []
 praktikans = []
+kelompoks = []
 total_as = 0
 total_prak = 0
 
 f = open('asistens.txt', 'r')
 for line in f:
     line = line.rstrip().split(';')
+    kelompoks.append(line[0])
     asistens.append({'kelompok':line[0], 'jml':int(line[1])})
     total_as += int(line[1])
 
@@ -27,6 +30,7 @@ maksimal = math.ceil(total_prak/total_as)
 minimal = math.floor(total_prak/total_as)
 plot = {}
 
+print('>>assigning praktikans to asisten')
 for asisten in asistens:
     for i in range(asisten['jml']):
         prak_plot = []
@@ -41,6 +45,7 @@ for asisten in asistens:
             del praktikans[randval-1]
         plot[asisten['kelompok']+str(i+1)] = prak_plot
 
+print('>>assigning last {} praktikans to asisten'.format(len(praktikans)))
 for prak in praktikans:
     randval = random.randint(0,5)
     kelompok = chr(65+randval)
@@ -52,9 +57,24 @@ for prak in praktikans:
         randval = random.randint(1,4)
     plot[kelompok+str(randval)].append(prak)
 
+plot_per_kelas = {}
+
+for kel in kelompoks:
+    plot_per_kelas[kel] = []
+    for p in plot:
+        for prak in plot[p]:
+            if(prak[0] == kel):
+                plot_per_kelas[kel].append([prak,p])
+
+for kel in kelompoks:
+    plot_per_kelas[kel] = sorted(plot_per_kelas[kel],key=itemgetter(0))
+
+print('>>writing result to output file')
 f = open('output.csv', 'w')
-f.writelines('kelompok;asisten;\n')
-for p in plot:
-    for prak in plot[p]:
-        f.writelines(prak+';'+p+';\n')
+for p in plot_per_kelas:
+    f.writelines('kelompok;asisten;\n')
+    for plot in plot_per_kelas[p]:
+        f.writelines(plot[0]+';'+plot[1]+';\n')
+    f.writelines('\n')
+print('done')
 f.close()
